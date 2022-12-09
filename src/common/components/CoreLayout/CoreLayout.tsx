9e1b5bed961948f11abccf8cac479c1f4f8cc2bf
Eric Wee <tzeweiwee@gmail.com>
2022-12-09 18:19:42 +0800

    updated bg color for pages

--- src/common/components/CoreLayout/CoreLayout.tsx
+++ src/common/components/CoreLayout/CoreLayout.tsx
@@ -3,7 +3,11 @@ import { Box } from '@chakra-ui/react';
 
 export const CoreLayout = ({ children }) => {
   return (
-    <Box minH="100vh" bgColor="#EFFAFD">
+    <Box
+      minH="100vh"
+      bg="url(.png), linear-gradient(180deg, rgba(239, 250, 253, 0.4) 0%, rgba(239, 250, 253, 0.6) 27.41%), #FFFFFF;"
+      bgBlendMode="overlay, normal, normal"
+    >
       <Navbar>{children}</Navbar>
     </Box>
   );
