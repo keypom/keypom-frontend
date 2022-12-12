000f54352180e9e8b7f5173a4fcc6fc98c47edcb
Eric Wee <tzeweiwee@gmail.com>
2022-12-13 01:00:25 +0800

    added footer

--- src/common/components/CoreLayout/CoreLayout.tsx
+++ src/common/components/CoreLayout/CoreLayout.tsx
@@ -1,14 +1,18 @@
 import { Navbar } from '@/common/components/Navbar/Navbar';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { Footer } from '@/common/components/Footer/Footer';
 
 export const CoreLayout = ({ children }) => {
   return (
-    <Box
+    <Flex
       minH="100vh"
+      flexDir="column"
       bg="url(.png), linear-gradient(180deg, rgba(239, 250, 253, 0.4) 0%, rgba(239, 250, 253, 0.6) 27.41%), #FFFFFF;"
       bgBlendMode="overlay, normal, normal"
     >
-      <Navbar>{children}</Navbar>
-    </Box>
+      <Navbar />
+      {children}
+      <Footer mt="auto" />
+    </Flex>
   );
 };
---
+++ src/common/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import { Box, ChakraProps, HStack, Image, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import { TwitterLogoIcon } from '@/common/components/Icons/TwitterLogoIcon';
+import NEAR_LOGO from 'assets/near_logo_wht.svg';
+
+interface IFooter extends ChakraProps {}
+
+const TWITTER_LINK = '';
+
+export const Footer: React.FC<IFooter> = (props) => {
+  return (
+    <Box bgColor="#222734" {...props}>
+      <HStack marginX="auto" maxW={[400, 1000]} h="100px" spacing="auto">
+        {/* Powered by NEAR */}
+        <Link href="https:www.twitter.com" target="_blank">
+          <HStack>
+            <Text color="white" fontSize="20px" whiteSpace="nowrap" mr="14px">
+              Powered by
+            </Text>
+            <Image src={NEAR_LOGO.src} alt="" w="95px" h="25px" />
+          </HStack>
+        </Link>
+
+        {/* Follow us Twitter */}
+        <Link href="https:www.twitter.com" target="_blank">
+          <HStack align="center" justify="center">
+            <TwitterLogoIcon h="24" w="27" mr="9px" mt="1px" />
+            <Text m={0} color="white" fontSize="20px">
+              Follow us
+            </Text>
+          </HStack>
+        </Link>
+      </HStack>
+    </Box>
+  );
+};
