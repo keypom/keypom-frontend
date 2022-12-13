f9ddf96b3557b920a982057933a91303fca51867
Eric Wee <tzeweiwee@gmail.com>
2022-12-13 16:59:30 +0800

    added colors to theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { CoreLayout } from '@/common/components/CoreLayout';
 import { PageHead } from '@/common/components/PageHead';
 
-import { theme } from '../../theme';
+import { theme } from '@/common/theme/theme';
 
 export const App = ({ Component, pageProps }) => {
   const Layout = Component.layout ? Component.layout : CoreLayout;
