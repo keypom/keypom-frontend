579c725ff29138786328c7668096543f010ff566
Eric <tzeweiwee@gmail.com>
2022-12-19 16:20:21 +0800

    feat: added links to navbar elements

--- src/common/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ src/common/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -5,7 +5,9 @@ type ConnectWalletButtonProps = ButtonProps;
 export const ConnectWalletButton = ({ ...props }: ConnectWalletButtonProps) => {
   return (
     <Button
+      as="a"
       fontSize={{ sm: 'sm', md: 'md' }}
+      href="/sign-in"
       px={{ sm: '2', md: '6' }}
       variant="primary"
       {...props}
