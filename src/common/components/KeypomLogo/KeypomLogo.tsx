a4dc2b2d058d02596c1ae7a58c6833dbf2f98c36
Eric <tzeweiwee@gmail.com>
2022-12-21 22:44:10 +0800

    fix: added missing a tag to logo click

--- src/common/components/KeypomLogo/KeypomLogo.tsx
+++ src/common/components/KeypomLogo/KeypomLogo.tsx
@@ -3,8 +3,8 @@ import NextLink from 'next/link';
 
 export const KeypomLogo = () => {
   return (
-    <NextLink href="/">
-      <HStack cursor="pointer" spacing={{ base: 2, md: '2.5' }}>
+    <NextLink passHref href="/">
+      <HStack as="a" cursor="pointer" spacing={{ base: 2, md: '2.5' }}>
         <Box bgColor="gray.800" h={{ base: 5, md: '7' }} rounded="full" w={{ base: 5, md: '7' }} />
         <Text as="b" fontSize={{ base: 'xl', md: '2xl' }}>
           Keypom
