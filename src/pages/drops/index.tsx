3ebcd99f72204c43454bbdd0789342ee9e0bbfdc
Eric <tzeweiwee@gmail.com>
2022-12-21 15:47:26 +0800

    fix: add mb to all drops page

--- src/pages/drops/index.tsx
+++ src/pages/drops/index.tsx
@@ -6,7 +6,7 @@ import AllDrops from '@/modules/AllDrops/AllDrops';
 
 export default function AllDropsPage() {
   return (
-    <Box minH="100%" minW="100%" mt={{ base: '52px', md: '100px' }}>
+    <Box mb={{ base: '5', md: '14' }} minH="100%" minW="100%" mt={{ base: '52px', md: '100px' }}>
       <PageHead
         removeTitleAppend
         description="Page containing all drops created by user"
