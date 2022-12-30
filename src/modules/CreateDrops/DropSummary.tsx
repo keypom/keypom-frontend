ab8e4d2dbb24c4a60fa6855c239f2292a1f3cb2e
Eric <tzeweiwee@gmail.com>
2022-12-30 21:33:46 +0800

    feat: added drop summary

---
+++ src/modules/CreateDrops/DropSummary.tsx
@@ -0,0 +1,14 @@
+import { LinkIcon } from '@chakra-ui/icons';
+
+import { RoundIcon } from '@/common/components/IconBox/RoundIcon';
+
+const data = {
+  dropName: 'Star Invasion Invite',
+  amountPerLink: '200',
+  totalLinks: '25',
+  redirectLink: 'starinvader.gg/login',
+};
+
+export const DropSummary = () => {
+  <RoundIcon icon={<LinkIcon />}></RoundIcon>;
+};
