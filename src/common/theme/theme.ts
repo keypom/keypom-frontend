f5abdda1920ddaa69912bb8037d165782f8a0ebb
Sushan Yadav <sushanyadav98@gmail.com>
2023-01-03 11:02:25 +0545

    nit: changed code changes postion for avoiding conflicts

--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -1,15 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 
-import { ButtonTheme, TableTheme, MenuTheme, BadgeTheme, ModalTheme } from './components';
+import { ModalTheme, ButtonTheme, TableTheme, MenuTheme, BadgeTheme } from './components';
 import { colors } from './colors';
 
 export const theme = extendTheme({
   components: {
+    Modal: ModalTheme,
     Button: ButtonTheme,
     Table: TableTheme,
     Menu: MenuTheme,
     Badge: BadgeTheme,
-    Modal: ModalTheme,
   },
   colors,
 });
