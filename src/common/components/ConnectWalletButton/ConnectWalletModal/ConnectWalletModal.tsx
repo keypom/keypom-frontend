7a0ed77d1e797a4e605196efe63ea12dfec5eed1
Sushan Yadav <sushanyadav98@gmail.com>
2023-01-03 13:03:24 +0545

    cleanup: removed icon from modal

--- src/common/components/ConnectWalletButton/ConnectWalletModal/ConnectWalletModal.tsx
+++ src/common/components/ConnectWalletButton/ConnectWalletModal/ConnectWalletModal.tsx
@@ -1,32 +1,23 @@
-import { LinkIcon } from '@chakra-ui/icons';
 import {
   Button,
   Modal,
   ModalContent,
   ModalOverlay,
-  Box,
   ModalHeader,
   ModalFooter,
   ModalBody,
 } from '@chakra-ui/react';
-
-import { RoundIcon } from '@/common/components/IconBox/RoundIcon';
 
 interface ConnectWalletModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const icon = <LinkIcon h={{ base: '8', md: '10' }} w={{ base: '8', md: '10' }} />;
-
 export const ConnectWalletModal = ({ isOpen, onClose }: ConnectWalletModalProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <Box left="50%" position="absolute" top="0" transform="translate(-50%, -50%)">
-          <RoundIcon icon={icon} />
-        </Box>
         <ModalHeader>Sign In</ModalHeader>
         <ModalBody>Sign in with a NEAR Wallet to create and manage drops.</ModalBody>
         <ModalFooter>
