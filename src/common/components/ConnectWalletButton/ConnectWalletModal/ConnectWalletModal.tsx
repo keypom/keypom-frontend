be8fa3fb6bd04821c16c42f21b3d3a68565e39d5
Sushan Yadav <sushanyadav98@gmail.com>
2023-01-03 14:00:50 +0545

    feat: added back te icons

--- src/common/components/ConnectWalletButton/ConnectWalletModal/ConnectWalletModal.tsx
+++ src/common/components/ConnectWalletButton/ConnectWalletModal/ConnectWalletModal.tsx
@@ -1,12 +1,16 @@
+import { LinkIcon } from '@chakra-ui/icons';
 import {
   Button,
   Modal,
   ModalContent,
   ModalOverlay,
+  Box,
   ModalHeader,
   ModalFooter,
   ModalBody,
 } from '@chakra-ui/react';
+
+import { RoundIcon } from '@/common/components/IconBox/RoundIcon';
 
 interface ConnectWalletModalProps {
   isOpen: boolean;
@@ -18,6 +22,9 @@ export const ConnectWalletModal = ({ isOpen, onClose }: ConnectWalletModalProps)
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
+        <Box left="50%" position="absolute" top="0" transform="translate(-50%, -50%)">
+          <RoundIcon icon={<LinkIcon h={{ base: '8', md: '10' }} w={{ base: '8', md: '10' }} />} />
+        </Box>
         <ModalHeader>Sign In</ModalHeader>
         <ModalBody>Sign in with a NEAR Wallet to create and manage drops.</ModalBody>
         <ModalFooter>
