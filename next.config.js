8b0281a3d81527f31427864549b3eee5b6d52546
Eric <tzeweiwee@gmail.com>
2023-01-05 15:04:45 +0800

    feat: test ssr build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  // experimental: { unable to build with Edge. See https://github.com/vercel/next.js/issues/40379
-  //   runtime: 'experimental-edge',
-  // },
+  experimental: {
+    // unable to build with Edge. See https://github.com/vercel/next.js/issues/40379
+    runtime: 'experimental-edge',
+  },
 };
