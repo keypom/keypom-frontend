48828eb4c55f90b415b8db539d6b192ee5d0976e
Eric <tzeweiwee@gmail.com>
2023-01-09 19:04:22 +0800

    feat: multistep form and create ticket contexts

---
+++ src/modules/CreateDrops/TicketDrop/CreateTicketDropContext.tsx
@@ -0,0 +1,69 @@
+import React, { createContext, PropsWithChildren, useContext } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import useSWRMutation from 'swr/mutation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import { PaymentData, SummaryItem } from '../types/types';
+
+const CreateTicketDropContext = createContext(null);
+
+const schema = z.object({
+  dropName: z.string().min(1, 'Drop name required'),
+});
+
+type Schema = z.infer<typeof schema>;
+
+// TODO: this is only a mock implementation of the backend api
+const createLinks = async () => {
+  await new Promise((res) => setTimeout(res, 2000));
+  return {
+    success: true,
+  };
+};
+
+/**
+ *
+ * Context for managing form state
+ */
+export const CreateTicketDropProvider = ({ children }: PropsWithChildren) => {
+  const { trigger, data } = useSWRMutation('/api/drops/tickets', createLinks);
+  const methods = useForm<Schema>({
+    mode: 'onChange',
+    defaultValues: {
+      dropName: '',
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const getSummaryData = (): SummaryItem[] => {
+    const { getValues } = methods;
+    const [dropName] = getValues(['dropName']);
+
+    return [];
+  };
+
+  const getPaymentData = (): PaymentData => {
+    return {};
+  };
+
+  const handleDropConfirmation = () => {
+    // TODO: send transaction/request to backend
+    trigger();
+  };
+
+  const createLinksSWR = {
+    data,
+    handleDropConfirmation,
+  };
+
+  return (
+    <CreateTicketDropContext.Provider
+      value={{ getSummaryData, getPaymentData, handleDropConfirmation, createLinksSWR }}
+    >
+      <FormProvider {...methods}>{children}</FormProvider>
+    </CreateTicketDropContext.Provider>
+  );
+};
+
+export const useCreateTicketDropContext = () => useContext(CreateTicketDropContext);
