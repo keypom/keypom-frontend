1f83000f14aa659a1a949a2223ea70d615020316
Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
2023-01-11 13:25:43 +0800

    feat: create nft drop flow (#44)
    * chore: add new image icon
    
    * feat: implement common FIleInput
    
    * feat: implement common text area input
    
    * chore: refactor drop summary to accommodate image type summary
    
    * fix: replace height with h to allow overwrite
    
    * feat: module specific fields
    
    * chore: common constant and update summary type
    
    * feat: implement create nft drop flow
    
    * chore: modify mobile look
    
    * chore: add helper message to TextInput common component
    
    * feat: implement redirect input module
    
    * chore: rename props
    
    * feat: add error message to inputs
    
    * refactor: drop summary item components
    
    * chore: pr review

--- src/common/components/TextInput/TextInput.tsx
+++ src/common/components/TextInput/TextInput.tsx
@@ -1,21 +1,48 @@
-import { Flex, FormControl, FormLabel, Input, InputGroup, InputProps } from '@chakra-ui/react';
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputProps,
+  Text,
+} from '@chakra-ui/react';
 
 interface TextInputProps extends InputProps {
   label: string;
+  topLeftHelperMessage?: string;
   errorMessage?: string;
 }
 
-export const TextInput = ({ label, errorMessage, ...props }: TextInputProps) => {
+export const TextInput = ({
+  label,
+  topLeftHelperMessage,
+  errorMessage,
+  ...props
+}: TextInputProps) => {
+  const haveTopHelperText = !!topLeftHelperMessage;
   return (
     <FormControl>
       <Flex alignItems="center" justifyContent="flex-start" w="full">
-        <FormLabel htmlFor={props.id || undefined} mb="2">
+        <FormLabel htmlFor={props?.id} mb={haveTopHelperText ? '0.5' : '2'}>
           {label}
         </FormLabel>
       </Flex>
+      {topLeftHelperMessage && (
+        <Flex alignItems="center" justifyContent="flex-start" w="full">
+          <Text color="gray.600" lineHeight="1.5rem" mb="2">
+            {topLeftHelperMessage}
+          </Text>
+        </Flex>
+      )}
       <InputGroup>
         <Input isInvalid={!!errorMessage} type="text" {...props} />
       </InputGroup>
+      {errorMessage && (
+        <Text fontSize={{ base: 'xs', md: 'sm' }} mt="6px" textAlign="left" variant="error">
+          {errorMessage}
+        </Text>
+      )}
     </FormControl>
   );
 };
---
+++ src/common/constant.ts
@@ -0,0 +1,2 @@
+// Matches with optional protocol and URL with one dot
+export const urlRegex = /(?:(?:https?:\/\/)?[\w.-]*\.[\w]{2,3})/;
