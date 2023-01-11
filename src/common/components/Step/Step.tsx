e7c24e517f480a8a0819046a6ac2a74a06d71dfd
Eric <tzeweiwee@gmail.com>
2023-01-11 23:40:28 +0800

    fix: move multi step into form

---
+++ src/common/components/Step/Step.tsx
@@ -0,0 +1,33 @@
+import { Box, HStack, Text } from '@chakra-ui/react';
+
+export interface StepItem {
+  title: string;
+  name: string;
+  component: React.ReactNode;
+  submitOnNextClick?: boolean;
+}
+export const Step = ({ index, stepItem, isActive }) => {
+  if (isActive) {
+    return (
+      <HStack key={stepItem.name} alignItems="center">
+        <Box bgColor="blue.400" borderRadius="100%" color="white" mr="2" px="2.5" py="0.5">
+          {index}
+        </Box>
+        <Text color="gray.800" whiteSpace="nowrap">
+          {stepItem.title}
+        </Text>
+      </HStack>
+    );
+  }
+
+  return (
+    <HStack key={stepItem.name}>
+      <Box bgColor="gray.100" borderRadius="100%" mr="2" px="2.5" py="0.5">
+        {index}
+      </Box>
+      <Text color="gray.600" whiteSpace="nowrap">
+        {stepItem.title}
+      </Text>
+    </HStack>
+  );
+};
--- src/modules/CreateDrops/TicketDrop/CreateTicketDropForm.tsx
+++ src/modules/CreateDrops/TicketDrop/CreateTicketDropForm.tsx
@@ -1,8 +1,10 @@
 import { useFormContext } from 'react-hook-form';
+import { Button, Divider, HStack } from '@chakra-ui/react';
 
 import { IconBox } from '@/common/components/IconBox';
 import { LinkIcon } from '@/common/components/Icons';
-import { MultiStepForm, StepItem } from '@/common/components/MultiStep/MultiStep';
+import { Step, StepItem } from '@/common/components/Step/Step';
+import { useSteps } from '@/common/hooks/useSteps';
 
 import { useDropFlowContext } from '../contexts/DropFlowContext';
 
@@ -40,12 +42,23 @@ const formSteps: StepItem[] = [
 export const CreateTicketDropForm = () => {
   const { onNext } = useDropFlowContext();
   const {
+    onNext: onNextStep,
+    onPrevious: onPreviousStep,
+    currentIndex,
+  } = useSteps({ maxSteps: formSteps.length });
+  const {
     setValue,
     handleSubmit,
     control,
     watch,
-    formState: { isDirty, isValid },
+    formState: { isDirty, isValid, errors },
   } = useFormContext();
+
+  console.log(errors);
+
+  const stepsDisplay = formSteps.map((step, index) => (
+    <Step key={step.name} index={index + 1} isActive={currentIndex === index} stepItem={step} />
+  ));
 
   const handleSubmitClick = () => {
     onNext();
@@ -57,7 +70,21 @@ export const CreateTicketDropForm = () => {
       maxW={{ base: '21.5rem', md: '36rem' }}
       mx="auto"
     >
-      <MultiStepForm steps={formSteps} />
+      <HStack flexWrap="nowrap" justifyContent="center" spacing="4">
+        {stepsDisplay}
+      </HStack>
+      {formSteps[currentIndex].component}
+      <Divider my={{ base: '6', md: '8' }} />
+      <HStack justifyContent="flex-end" spacing="auto">
+        {currentIndex > 0 && (
+          <Button variant="secondary" onClick={onPreviousStep}>
+            Go back
+          </Button>
+        )}
+        <Button variant="secondary" onClick={onNext}>
+          Skip this step
+        </Button>
+      </HStack>
     </IconBox>
   );
 };
