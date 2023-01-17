b40704336f354d589eaa77192ad52e96de021c3e
Andre H <andrehadiantolesmana@gmail.com>
2023-01-18 01:03:55 +0800

    chore: add comment

--- src/common/components/GradientSpan/GradientSpan.tsx
+++ src/common/components/GradientSpan/GradientSpan.tsx
@@ -4,6 +4,12 @@ import { PropsWithChildren } from 'react';
 interface GradientSpanProps {
   color?: string;
 }
+
+/**
+ *
+ * @param color:color of the text in the span. By default, will give the
+ * rainbow color gradient used in the landing page
+ */
 export const GradientSpan = ({
   children,
   color = 'linear-gradient(97.7deg, #FB50BF 14%, #00AEDF 49.33%)',
