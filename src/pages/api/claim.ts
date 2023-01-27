bf8774b5eed610d9bc47c48562b7f44db1a7f31f
Andre H <andrehadiantolesmana@gmail.com>
2023-01-27 15:52:47 +0800

    chore: use placeholder

--- src/pages/api/claim.ts
+++ src/pages/api/claim.ts
@@ -7,25 +7,22 @@ type DropInformation = {
   drop_id: string;
 }
 
-/**
- * 
- * @param req 
- * @param res 
- */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse<DropInformation>>  
 ) {
+  //TODO: call getDropInformation
+  const drop_info = {} // insert ProtocolReturnedDrop
+
   //TODO: to be replaced with type inference from drop id
   const type = req.query["type"] as string
 
+  // will be used to redirect user accordingly
+  drop_info['type'] = type
+
   const return_data = {
     drop_id: "sample_id_string",
-
-    drop_data: {
-      // will be used to redirect user accordingly
-      type
-    },
+    drop_info
   }
 
   res.status(200).json({data: return_data})
