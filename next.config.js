8df11e41927d0aa78e87461ba85359b4e3561ee1
Wahyu Kurniawan <ayungavis@gmail.com>
2023-01-30 10:19:19 +0700

    🔧 chore: update `next.config.js` to fix module not found

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
   reactStrictMode: true,
   // experimental: { unable to build with Edge. See https://github.com/vercel/next.js/issues/40379
   //   runtime: 'experimental-edge',
