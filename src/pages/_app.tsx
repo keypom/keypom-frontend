ace4cd45db2d50c3a7345992f34526ab72d5bf81
Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
2023-01-30 19:02:49 +0800

    feat: implement temporary claim api (#84)
    * feat: implement temporary claim api
    
    * chore: add apiresponse type
    
    * chore: use placeholder
    
    * build: add keypom-js
    
    * chore: _app to init keypomsdk
    
    * feat: implement claim api
    
    * build: add lodash

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { initKeypom } from 'keypom-js';
 
 import { CoreLayout } from '@/common/components/CoreLayout';
 import { PageHead } from '@/common/components/PageHead';
@@ -7,6 +8,15 @@ import { AuthWalletContextProvider } from '@/common/contexts/AuthWalletContext';
 
 import '@near-wallet-selector/modal-ui/styles.css';
 import '@/common/components/WalletSelectorModal/WalletSelectorModal.css';
+
+// Initialize the SDK on testnet.
+initKeypom({
+  network: process.env.NEXT_PUBLIC_KEYPOM_NETWORK,
+  funder: {
+    accountId: process.env.NEXT_PUBLIC_KEYPOM_ACC_ID,
+    secretKey: process.env.NEXT_PUBLIC_KEYPOM_SEC_KEY,
+  },
+});
 
 export const App = ({ Component, pageProps }) => {
   const Layout = Component.layout ? Component.layout : CoreLayout;
