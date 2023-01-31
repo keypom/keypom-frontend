835103821d1ac05f8b72fb2bca47259bd2606cf8
Matt <mattdlockyer@gmail.com>
2023-01-30 19:24:54 -0800

    prettier pass local

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,10 @@
-
 export const config = {
   runtime: 'edge',
 };
 
 const Component = () => {
-	return <h1>Test</h1>
-}
+  return <h1>Test</h1>;
+};
 
 export default function Page() {
   return <Component />;
