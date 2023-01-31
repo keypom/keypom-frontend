ec3c8131a1f95c10ec4f08ebcd4b46a11b7dd22d
Wahyu Kurniawan <ayungavis@gmail.com>
2023-01-31 17:40:34 +0700

    ♻️ refactor: remove all, starting to migrate

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  // experimental: { unable to build with Edge. See https://github.com/vercel/next.js/issues/40379
-  //   runtime: 'experimental-edge',
-  // },
-};
--- src/common/utils/replaceSpace.ts
+++
@@ -1,2 +0,0 @@
-export const replaceSpace = (text: string, replaceStr = '_'): string =>
-  text.replace(/\s/g, replaceStr);
