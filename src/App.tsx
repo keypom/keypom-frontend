f9a67eedb2670965e73f4c8aa7daefe042f2d941
Wahyu Kurniawan <ayungavis@gmail.com>
2023-01-31 20:57:47 +0700

    🚀 perf: remove `react-topbar-progress-indicator` for minimize bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
-import TopBarProgress from 'react-topbar-progress-indicator';
 
 import { router } from './router';
+import { Loading } from './common/components/Loading';
 
 export const App = () => {
   return (
     <ChakraProvider>
-      <React.Suspense fallback={<TopBarProgress />}>
+      <React.Suspense fallback={<Loading />}>
         <RouterProvider router={router} />
       </React.Suspense>
     </ChakraProvider>
