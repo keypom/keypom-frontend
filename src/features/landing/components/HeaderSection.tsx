97809336684eab3d025b3d459c67b995cae9c7b6
Andre H <andrehadiantolesmana@gmail.com>
2023-02-01 17:10:11 +0800

    feat: implement landing page

---
+++ src/features/landing/components/HeaderSection.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Center, Heading, HStack, Text } from '@chakra-ui/react';
+
+import { GradientSpan } from '@/components/GradientSpan';
+import { LogoWhite } from '@/components/Icons';
+
+export const HeaderSection = () => {
+  return (
+    <Center flexDir="column" maxW="600px" mx="auto" py={{ base: '10', md: '16' }}>
+      <Heading
+        fontSize={{ base: '32px', md: '60px' }}
+        fontWeight="600"
+        lineHeight={{ base: '43px', md: '72px' }}
+        mb={{ base: '4', md: '6' }}
+        textAlign="center"
+      >
+        <GradientSpan>Instant</GradientSpan> crypto experiences
+      </Heading>
+      <Text
+        fontSize={{ base: 'lg', md: 'xl' }}
+        lineHeight="28px"
+        mb={{ base: '6', md: '8' }}
+        textAlign="center"
+      >
+        {`Drop tokens, experiences, and more. Let's onboard the masses with the click of a link.`}
+      </Text>
+      {/* Powered by NEAR */}
+      <HStack>
+        <Text color="gray.400" fontSize={{ base: 'md', md: 'lg' }}>
+          Powered by
+        </Text>
+        <LogoWhite h="25px" w="95px" />
+      </HStack>
+    </Center>
+  );
+};
