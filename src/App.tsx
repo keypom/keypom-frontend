dcf576354c647a28106a60a345c4ff34e52da8ac
Andre H <andrehadiantolesmana@gmail.com>
2023-02-01 17:23:00 +0800

    chore: add authwalletcontext provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 
+import { AuthWalletContextProvider } from './contexts/AuthWalletContext';
+
 import { Fonts } from '@/components/Fonts';
 import { Loading } from '@/components/Loading';
 import { theme } from '@/theme';
@@ -18,7 +20,9 @@ export const App = () => {
     <React.Suspense fallback={<Loading />}>
       <ChakraProvider theme={theme}>
         <Fonts />
-        <RouterProvider router={router} />
+        <AuthWalletContextProvider>
+          <RouterProvider router={router} />
+        </AuthWalletContextProvider>
       </ChakraProvider>
     </React.Suspense>
   );
