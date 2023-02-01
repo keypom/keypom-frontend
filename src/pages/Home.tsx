a52d4469547fc5e05ffcfeb4c0a02d34e56f5222
Andre H <andrehadiantolesmana@gmail.com>
2023-02-01 13:15:57 +0800

    chore: temporary text showcase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Heading, VStack } from '@chakra-ui/react';
+import { Button, Container, Heading, Text, VStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
@@ -16,6 +16,12 @@ export default function HomePage() {
         >
           Go to about page
         </Button>
+        <Text fontWeight="400">This is supposed to be Inter 400</Text>
+        <Text fontWeight="500">This is supposed to be Inter 500</Text>
+        <Text fontWeight="600">This is supposed to be Inter 600</Text>
+        <Heading fontWeight="400">This is supposed to be Archia 400</Heading>
+        <Heading fontWeight="500">This is supposed to be Archia 500</Heading>
+        <Heading fontWeight="600">This is supposed to be Archia 600</Heading>
       </VStack>
     </Container>
   );
