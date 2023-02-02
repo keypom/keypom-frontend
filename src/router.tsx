917b78e0f3bcdcd7870885f16ac024ae39992623
Andre H <andrehadiantolesmana@gmail.com>
2023-02-02 17:14:44 +0800

    feat: set routes

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,11 @@ import { CoreLayout } from '@/components/CoreLayout';
 
 const HomePage = React.lazy(async () => await import('./pages/Home'));
 const AboutPage = React.lazy(async () => await import('./pages/About'));
-const LandingPage = React.lazy(async () => await import('./features/landing'));
+const LandingPage = React.lazy(async () => await import('./features/landing/routes/LandingPage'));
+const ClaimTokenPage = React.lazy(async () => await import('./features/claim/routes/token'));
+const ClaimNftPage = React.lazy(async () => await import('./features/claim/routes/nft'));
+const ClaimGiftPage = React.lazy(async () => await import('./features/claim/routes/gift'));
+const ClaimTicketPage = React.lazy(async () => await import('./features/claim/routes/ticket/[id]'));
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +26,33 @@ export const router = createBrowserRouter([
       {
         path: '/landing',
         element: <LandingPage />,
+      },
+
+      {
+        path: '/claim',
+        children: [
+          {
+            path: 'token',
+            element: <ClaimTokenPage />,
+          },
+          {
+            path: 'nft',
+            element: <ClaimNftPage />,
+          },
+          {
+            path: 'gift',
+            element: <ClaimGiftPage />,
+          },
+          {
+            path: 'ticket',
+            children: [
+              {
+                path: ':id',
+                element: <ClaimTicketPage />,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
