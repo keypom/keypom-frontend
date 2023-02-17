375f47b65b3c7d30ce709e77d09b7068d8c14b56
Andre H <andrehadiantolesmana@gmail.com>
2023-02-17 14:13:50 +0800

    feat: integrate and paginate nft/[id]

--- src/features/drop-manager/routes/nft/[id].tsx
+++ src/features/drop-manager/routes/nft/[id].tsx
@@ -1,18 +1,15 @@
 import { Badge, Box, Button, Skeleton, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { generateKeys, getDropInformation, getKeyInformationBatch } from 'keypom-js';
 
 import { CopyIcon, DeleteIcon } from '@/components/Icons';
 import { DropManager } from '@/features/drop-manager/components/DropManager';
 import { type ColumnItem } from '@/components/Table/types';
-
-interface NFTDropResponse {
-  name: string;
-  links: Array<{
-    id: number;
-    slug: string;
-    hasClaimed: boolean;
-  }>;
-}
+import { useAuthWalletContext } from '@/contexts/AuthWalletContext';
+import { get } from '@/utils/localStorage';
+import { MASTER_KEY, PAGE_SIZE_LIMIT } from '@/constants/common';
+import { usePagination } from '@/hooks/usePagination';
 
 const tableColumns: ColumnItem[] = [
   { title: 'Link', selector: (row) => row.link, loadingElement: <Skeleton height="30px" /> },
@@ -33,25 +30,88 @@ const tableColumns: ColumnItem[] = [
   },
 ];
 
-const data: NFTDropResponse = {
-  name: 'Star Invader 3',
-  links: [
-    { id: 1, slug: '#2138h823h', hasClaimed: true },
-    { id: 2, slug: '#2138h823h', hasClaimed: false },
-    { id: 3, slug: '#c34fd2n32', hasClaimed: false },
-    { id: 4, slug: '#rf5hhfaxm', hasClaimed: true },
-  ],
-};
-
 export default function NFTDropManagerPage() {
+  const { id: dropId } = useParams();
   const [loading, setLoading] = useState(true);
 
-  // TODO: Remove this after backend is ready
+  const [name, setName] = useState('Drop');
+  const [dataSize, setDataSize] = useState<number>(0);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      link: 'https://example.com',
+      slug: 'https://example.com',
+      hasClaimed: false,
+      action: 'delete',
+    },
+  ]);
+
+  const { accountId } = useAuthWalletContext();
+
+  const {
+    hasPagination,
+    pagination,
+    firstPage,
+    lastPage,
+    loading: paginationLoading,
+    handleNextPage,
+    handlePrevPage,
+  } = usePagination({
+    dataSize,
+    handlePrevApiCall: async () => {
+      await handleGetDrops({
+        pageIndex: pagination.pageIndex - 1,
+        pageSize: pagination.pageSize,
+      });
+    },
+    handleNextApiCall: async () => {
+      await handleGetDrops({
+        pageIndex: pagination.pageIndex + 1,
+        pageSize: pagination.pageSize,
+      });
+    },
+  });
+
+  const handleGetDrops = async ({ pageIndex = 0, pageSize = PAGE_SIZE_LIMIT }) => {
+    if (!accountId) return;
+
+    const drop = await getDropInformation({
+      dropId,
+    });
+
+    setDataSize(drop.next_key_id);
+
+    setName(JSON.parse(drop.metadata as string).dropName);
+
+    const { publicKeys, secretKeys } = await generateKeys({
+      numKeys: Math.min(drop.next_key_id, pageSize),
+      rootEntropy: `${get(MASTER_KEY) as string}-${dropId}`,
+      autoMetaNonceStart: pageIndex * pageSize,
+    });
+
+    const keyInfo = await getKeyInformationBatch({
+      publicKeys,
+      secretKeys,
+    });
+    console.log('keyInfo', keyInfo);
+    console.log('secretKeys', secretKeys);
+
+    setData(
+      secretKeys.map((key, i) => ({
+        id: i,
+        link: 'https://keypom.xyz/claim/' + key.replace('ed25519:', ''),
+        slug: key.substring(8, 16),
+        hasClaimed: keyInfo[i] === null,
+        action: 'delete',
+      })),
+    );
+
+    setLoading(false);
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-  }, []);
+    handleGetDrops({});
+  }, [accountId]);
 
   // TODO: consider moving these to DropManager if backend request are the same for NFT and Ticket
   const handleCopyClick = () => {
@@ -62,9 +122,9 @@ export default function NFTDropManagerPage() {
   };
 
   const getTableRows = () => {
-    if (data === undefined || data?.links === undefined) return [];
+    if (data === undefined) return [];
 
-    return data.links.map((item) => ({
+    return data.map((item) => ({
       ...item,
       link: (
         <Text color="gray.400" display="flex">
@@ -99,8 +159,17 @@ export default function NFTDropManagerPage() {
           claimedHeaderText="NFT editions claimed"
           claimedText="200/500"
           data={getTableRows()}
-          dropName={data.name}
+          dropName={name}
           loading={loading}
+          pagination={{
+            hasPagination,
+            id: 'token',
+            paginationLoading,
+            firstPage,
+            lastPage,
+            handleNextPage,
+            handlePrevPage,
+          }}
           showColumns={false}
           tableColumns={tableColumns}
         />
