370185997be8dabddcb57af89d0ea0297b10173b
Eric <tzeweiwee@gmail.com>
2023-02-21 22:21:55 +0800

    feat: added qr code download

--- src/features/claim/components/ticket/[id]/QrDetails.tsx
+++ src/features/claim/components/ticket/[id]/QrDetails.tsx
@@ -4,10 +4,29 @@ import QRCode from 'react-qr-code';
 interface QrDetailsProps {
   qrValue: string;
   ticketName: string;
-  onClick?: () => void;
 }
 
-export const QrDetails = ({ qrValue, ticketName, onClick }: QrDetailsProps) => {
+export const QrDetails = ({ qrValue, ticketName }: QrDetailsProps) => {
+  const handleDownloadQrCode = () => {
+    const svg = document.getElementById('QRCode');
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const pngFile = canvas.toDataURL('image/png');
+      const downloadLink = document.createElement('a');
+      downloadLink.download = 'QRCode';
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  };
+
   return (
     <Flex align="center" flexDir="column" p={{ base: '6', md: '8' }} pt={{ base: '12', md: '16' }}>
       <Box
@@ -17,7 +36,7 @@ export const QrDetails = ({ qrValue, ticketName, onClick }: QrDetailsProps) => {
         mb={{ base: '6', md: '10' }}
         p="5"
       >
-        <QRCode size={120} value={qrValue} />
+        <QRCode id="QRCode" size={180} value={qrValue} />
       </Box>
       <Text
         color="gray.800"
@@ -31,7 +50,7 @@ export const QrDetails = ({ qrValue, ticketName, onClick }: QrDetailsProps) => {
       <Text color="gray.600" mb="6" size={{ base: 'sm', md: 'md' }} textAlign="center">
         A copy has been sent to your email. Save this QR code and show it at the event.{' '}
       </Text>
-      <Button variant="outline" w="full" onClick={onClick}>
+      <Button variant="outline" w="full" onClick={handleDownloadQrCode}>
         Download QR code
       </Button>
     </Flex>
