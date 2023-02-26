93ddd623a875ee409a5b63c4248eccf9164cdd34
Andre H <andrehadiantolesmana@gmail.com>
2023-02-26 23:02:28 +0800

    chore: add link to homepage

--- src/components/NotFound404/NotFound404.tsx
+++ src/components/NotFound404/NotFound404.tsx
@@ -1,19 +1,39 @@
-import { Center, Divider, Hide, HStack, Show, Text, VStack } from '@chakra-ui/react';
+import { Button, Center, Divider, Hide, HStack, Show, Text, VStack } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound404 = () => {
+  const navigate = useNavigate();
   return (
     <Center h="calc(100vh - 64px)">
       <Show above="md">
-        <HStack gap="4">
-          <Text size="4xl">404</Text>
-          <Divider borderColor="black" h="12" orientation="vertical" w="1" />
-          <Text size="md">This page could not be found.</Text>
-        </HStack>
+        <VStack spacing="40px">
+          <HStack gap="4">
+            <Text size="4xl">404</Text>
+            <Divider borderColor="black" h="12" orientation="vertical" w="1" />
+            <Text size="md">This page could not be found.</Text>
+          </HStack>
+          <Button
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Back to homepage
+          </Button>
+        </VStack>
       </Show>
       <Hide above="md">
         <VStack>
           <Text size="4xl">404</Text>
-          <Text size="md">This page could not be found.</Text>
+          <Text pb="20px" size="md">
+            This page could not be found.
+          </Text>
+          <Button
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Back to homepage
+          </Button>
         </VStack>
       </Hide>
     </Center>
