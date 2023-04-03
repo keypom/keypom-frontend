12cf7fb34d65b49ea2a3116e12971a5ccfead0f9
Andre H <andrehadiantolesmana@gmail.com>
2023-04-03 22:00:32 +0800

    feat: event drop do not need to check for master key

--- src/hooks/useValidMasterKey.tsx
+++ src/hooks/useValidMasterKey.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import { MASTER_KEY } from '@/constants/common';
 import { get } from '@/utils/localStorage';
+import keypomInstance from '@/lib/keypom';
 
 interface useValidMasterKeyProps {
   dropId: string;
@@ -13,18 +14,25 @@ export const useValidMasterKey = ({ dropId }: useValidMasterKeyProps) => {
 
   useEffect(() => {
     const validateMasterKey = async () => {
-      const keys = await getKeysForDrop({ dropId, start: 0, limit: 1 });
-      if (keys.length === 0) {
-        setValid(true); // assume all keys have been claimed
-        return;
-      }
-      const { publicKeys } = await generateKeys({
-        numKeys: 1,
-        rootEntropy: `${get(MASTER_KEY) as string}-${dropId}`,
-        metaEntropy: keys[0].key_id.toString(),
-      });
+      const drop = await keypomInstance.getDropInfo({ dropId });
+      const { eventId } = keypomInstance.getDropMetadata(drop.metadata);
+      if (eventId) {
+        // if event drop, do not need to check for masterKey
+        setValid(true);
+      } else {
+        const keys = await getKeysForDrop({ dropId, start: 0, limit: 1 });
+        if (keys.length === 0) {
+          setValid(true); // assume all keys have been claimed
+          return;
+        }
+        const { publicKeys } = await generateKeys({
+          numKeys: 1,
+          rootEntropy: `${get(MASTER_KEY) as string}-${dropId}`,
+          metaEntropy: keys[0].key_id.toString(),
+        });
 
-      setValid(keys[0].pk === publicKeys[0]);
+        setValid(keys[0].pk === publicKeys[0]);
+      }
     };
     validateMasterKey();
   }, []);
