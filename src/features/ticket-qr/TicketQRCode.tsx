10498e0d53934a6f04a94594c01b6b9c6af42910
zach <zdefranc@uwaterloo.ca>
2024-03-08 15:08:18 -0500

    Removed qr-image package

--- src/features/ticket-qr/TicketQRCode.tsx
+++ src/features/ticket-qr/TicketQRCode.tsx
@@ -1,14 +1,8 @@
 import { Box, Image, Text, Center, VStack } from '@chakra-ui/react';
-import { imageSync } from 'qr-image';
 import { useParams } from 'react-router-dom';
+import QRCode from 'react-qr-code';
 
 export default function TicketQrCode() {
-
-    function getQRCodeBase64(ticketKey: string): string {
-        var qrImagePng = imageSync(ticketKey, { type: 'png' });
-        // Convert the QR code image to base64
-        return "data:image/png;base64," + qrImagePng.toString('base64');
-    }
 
     const { ticketKey } = useParams<{ ticketKey: string }>();
     // console.log(useParams<{ ticketId: string }>());
@@ -18,7 +12,7 @@ export default function TicketQrCode() {
         <VStack>
             <Text paddingTop="5rem" paddingBottom="1rem" fontWeight="500" size="xl">Your QR code ticket is below. Enjoy your event!</Text>
             <Center>
-                <Image alt="keypom-logo" src={getQRCodeBase64(ticketKey)} w={{ base: '150px', md: '300px' }} />
+                <QRCode value={ticketKey} />
             </Center>
         </VStack>
     ) : (
