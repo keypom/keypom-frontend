398cb48b085b7dc6fe05a5b05b945f1ffd82e122
BenKurrek <bkurrek@uwaterloo.ca>
2024-03-08 17:14:42 -0500

    implemented error checking and skeleton loaders

--- src/features/ticket-qr/TicketQRPage.tsx
+++ src/features/ticket-qr/TicketQRPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex, Heading, Hide, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Hide, Image, Skeleton, Text, VStack } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { getPubFromSecret } from 'keypom-js';
 
@@ -12,7 +12,7 @@ import keypomInstance from '@/lib/keypom';
 import { type FunderEventMetadata, type EventDropMetadata } from '@/lib/eventsHelpers';
 
 export default function TicketQRPage() {
-  const { dropId, secretKey } = useTicketClaimParams();
+  const { secretKey } = useTicketClaimParams();
 
   const [isValid, setIsValid] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
@@ -23,36 +23,33 @@ export default function TicketQRPage() {
 
   useEffect(() => {
     const getEventInfo = async () => {
-      setIsLoading(true);
-      const pubKey = getPubFromSecret(secretKey);
-      const keyInfo: { drop_id: string } = await keypomInstance.viewCall({
-        methodName: 'get_key_information',
-        args: { key: pubKey },
-      });
-      const drop: { drop_id: string; funder_id: string; drop_config: { metadata: string } } =
-        await keypomInstance.viewCall({
-          methodName: 'get_drop_information',
-          args: { drop_id: keyInfo.drop_id },
+      try {
+        setIsLoading(true);
+        const pubKey = getPubFromSecret(secretKey);
+        const keyInfo: { drop_id: string } = await keypomInstance.viewCall({
+          methodName: 'get_key_information',
+          args: { key: pubKey },
         });
-      const ticketMetadata: EventDropMetadata = JSON.parse(drop.drop_config.metadata);
-      const eventInfo: FunderEventMetadata = await keypomInstance.getEventInfo({
-        accountId: drop.funder_id,
-        eventId: ticketMetadata.eventId,
-      });
-      console.log('eventInfo', eventInfo);
-      console.log('ticketMetadata', ticketMetadata);
-      setTicketName(ticketMetadata.name);
-      setEventImage(eventInfo.artwork);
-      setEventName(eventInfo.name);
-      setIsLoading(false);
+        const drop: { drop_id: string; funder_id: string; drop_config: { metadata: string } } =
+          await keypomInstance.viewCall({
+            methodName: 'get_drop_information',
+            args: { drop_id: keyInfo.drop_id },
+          });
+        const ticketMetadata: EventDropMetadata = JSON.parse(drop.drop_config.metadata);
+        const eventInfo: FunderEventMetadata = await keypomInstance.getEventInfo({
+          accountId: drop.funder_id,
+          eventId: ticketMetadata.eventId,
+        });
+        setTicketName(ticketMetadata.name);
+        setEventImage(eventInfo.artwork);
+        setEventName(eventInfo.name);
+        setIsLoading(false);
+      } catch (e) {
+        console.error('Error getting event info: ', e);
+        setIsValid(false);
+      }
     };
-    try {
-      getEventInfo();
-    } catch (e) {
-      console.error('Error getting event info: ', e);
-
-      setIsValid(false);
-    }
+    getEventInfo();
   }, []);
 
   if (!isValid) {
@@ -68,12 +65,18 @@ export default function TicketQRPage() {
         paddingBottom="20"
         paddingTop="20"
       >
-        <Heading fontSize={{ base: '2xl', md: '3xl' }} fontWeight="500" textAlign="center">
-          {ticketName}
-        </Heading>
+        <Skeleton fadeDuration={1} isLoaded={!isLoading}>
+          <Heading fontSize={{ base: '2xl', md: '3xl' }} fontWeight="500" textAlign="center">
+            {isLoading ? 'Loading ticket...' : ticketName}
+          </Heading>
+        </Skeleton>
 
         <IconBox
-          icon={<TicketIcon height={{ base: '8', md: '10' }} width={{ base: '8', md: '10' }} />}
+          icon={
+            <Skeleton isLoaded={!isLoading}>
+              <TicketIcon height={{ base: '8', md: '10' }} width={{ base: '8', md: '10' }} />
+            </Skeleton>
+          }
           maxW={{ base: '345px', md: '30rem' }}
           minW={{ base: 'inherit', md: '345px' }}
           p="0"
@@ -81,7 +84,11 @@ export default function TicketQRPage() {
         >
           <Box>
             <BoxWithShape bg="white" borderTopRadius="8xl" w="full">
-              <QrDetails eventName={eventName} qrValue={secretKey} ticketName={ticketName} />
+              {isLoading ? (
+                <Skeleton height="200px" width="full" />
+              ) : (
+                <QrDetails eventName={eventName} qrValue={secretKey} ticketName={ticketName} />
+              )}
             </BoxWithShape>
             <Flex
               align="center"
@@ -91,22 +98,26 @@ export default function TicketQRPage() {
               px="6"
               py="8"
             >
-              <Image
-                alt={`Event image for ${ticketName}`}
-                borderRadius="12px"
-                objectFit="contain"
-                src={eventImage}
-              />
+              <Skeleton borderRadius="12px" isLoaded={!isLoading}>
+                <Image
+                  alt={`Event image for ${ticketName}`}
+                  borderRadius="12px"
+                  objectFit="contain"
+                  src={eventImage}
+                />
+              </Skeleton>
               <Hide above="md">
-                <Text
-                  color="gray.600"
-                  fontWeight="600"
-                  mb="2"
-                  size={{ base: 'sm', md: 'md' }}
-                  textAlign="center"
-                >
-                  {eventName}
-                </Text>
+                <Skeleton isLoaded={!isLoading} mt="4">
+                  <Text
+                    color="gray.600"
+                    fontWeight="600"
+                    mb="2"
+                    size={{ base: 'sm', md: 'md' }}
+                    textAlign="center"
+                  >
+                    {eventName}
+                  </Text>
+                </Skeleton>
               </Hide>
             </Flex>
           </Box>
