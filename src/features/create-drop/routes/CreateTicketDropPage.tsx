62955aec5579f31f6bd308a23a0eacecf408ecac
BenKurrek <bkurrek@uwaterloo.ca>
2024-03-24 00:35:50 -0400

    fixed clearing details

--- src/features/create-drop/routes/CreateTicketDropPage.tsx
+++ src/features/create-drop/routes/CreateTicketDropPage.tsx
@@ -141,74 +141,6 @@ const formSteps: TicketStep[] = [
   },
 ];
 
-// const placeholderData2 = {
-//   eventName: { value: 'Vandelay Industries Networking Event' },
-//   eventArtwork: { value: undefined },
-//   eventDescription: {
-//     value:
-//       'Meet with the best latex salesmen in the industry! This will be a once-in-a-lifetime opportunity to network and meet with people that you enjoy being with. Drink beer, laugh, have fun, and have a great time at this networking event! This will be a once-in-a-lifetime opportunity!',
-//   },
-//   eventLocation: { value: '129 West 81st Street, Apartment Suite 288.' },
-//   date: {
-//     value: {
-//       value: {
-//         value: { startDate: null, endDate: null },
-//         startDate: '2024-03-16T04:00:00.000Z',
-//         endDate: null,
-//       },
-//       startDate: new Date('2024-03-16T04:00:00.000Z'),
-//       endDate: new Date('2024-03-23T04:00:00.000Z'),
-//       startTime: '09:00 AM',
-//       endTime: '09:00 PM',
-//     },
-//   },
-//   questions: [
-//     { question: 'Full name', isRequired: true },
-//     { question: 'Email address', isRequired: true },
-//     { question: 'How did you find out about this event?', isRequired: false },
-//   ],
-//   tickets: [
-//     {
-//       name: 'VIP Ticket',
-//       description:
-//         'Get exclusive access to beer, fun, and games. Network with people, grant priority access to lines and skip the wait with this amazing ticket tier!',
-//       price: '5',
-//       artwork: undefined,
-//       salesValidThrough: {
-//         startDate: new Date('2024-03-13T04:00:00.000Z'),
-//         endDate: new Date('2024-03-23T04:00:00.000Z'),
-//       },
-//       passValidThrough: {
-//         startDate: new Date('2024-03-18T04:00:00.000Z'),
-//         endDate: new Date('2024-03-22T04:00:00.000Z'),
-//       },
-//       maxSupply: 5000,
-//     },
-//     {
-//       name: 'Standard Ticket',
-//       description: 'Get standard access to the networking event',
-//       artwork: undefined,
-//       price: '0',
-//       salesValidThrough: {
-//         startDate: new Date('2024-03-13T04:00:00.000Z'),
-//         endDate: new Date('2024-03-23T04:00:00.000Z'),
-//       },
-//       passValidThrough: {
-//         startDate: new Date('2024-03-18T04:00:00.000Z'),
-//         endDate: new Date('2024-03-22T04:00:00.000Z'),
-//       },
-//       maxSupply: 5000,
-//     },
-//   ],
-//   actions: [],
-//   costBreakdown: {
-//     perEvent: '0',
-//     perDrop: '0',
-//     total: '0',
-//     marketListing: '0',
-//   },
-// };
-
 const placeholderData: TicketDropFormData = {
   // Step 0
   nearPrice: undefined,
@@ -303,13 +235,16 @@ export default function NewTicketDrop() {
 
   const handleClearForm = () => {
     switch (currentStep) {
-      case 0:
+      case 1:
         setFormData((prev) => ({ ...prev, ...ClearEventInfoForm() }));
         break;
-      case 1:
-        setFormData((prev) => ({ ...prev, questions: [] }));
-        break;
       case 2:
+        setFormData((prev) => ({
+          ...prev,
+          questions: [{ question: EMAIL_QUESTION, isRequired: true }],
+        }));
+        break;
+      case 3:
         setFormData((prev) => ({ ...prev, tickets: [] }));
         break;
       default:
@@ -413,7 +348,10 @@ export default function NewTicketDrop() {
   const handleSkipped = () => {
     setCurrentStep((prevStep) => prevStep + 1);
     if (currentStep === 2) {
-      setFormData((prev) => ({ ...prev, questions: [] }));
+      setFormData((prev) => ({
+        ...prev,
+        questions: [{ question: EMAIL_QUESTION, isRequired: true }],
+      }));
     }
   };
 
