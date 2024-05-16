ecd2a8c6e47ecc92f3f7b297c9130fe2c9df1bf1
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2024-05-16 15:04:17 -0400

    quick import fix

--- src/features/create-drop/components/ticket/helpers.tsx
+++ src/features/create-drop/components/ticket/helpers.tsx
@@ -1,5 +1,5 @@
 import { type Action } from '@near-wallet-selector/core';
-import { parseNearAmount } from '@keypom/core';
+import { parseNearAmount } from 'near-api-js/lib/utils/format';
 
 import keypomInstance from '@/lib/keypom';
 import {
